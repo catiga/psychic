c9582fc61beccced8027077726811e59f0353415
baiqi <495199985@qq.com>
2024-03-10 23:54:35 +0800

    fix bug

--- app/model/account_user_info.gen.go
+++ app/model/account_user_info.gen.go
@@ -15,7 +15,7 @@ type AccountUserInfo struct {
 	ID       int64     `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
 	Name     string    `gorm:"column:name" json:"name"`
 	Address  string    `gorm:"column:address" json:"address"`
-	CreateAt time.Time `gorm:"column:create_at" json:"create_at"`
+	CreateAt *time.Time `gorm:"column:create_at" json:"create_at"`
 	Type     int32     `gorm:"column:type" json:"type"`
 	IP       string    `gorm:"column:ip" json:"ip"`
 	Kyc      int32     `gorm:"column:kyc" json:"kyc"`
